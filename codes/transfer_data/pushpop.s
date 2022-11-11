6a19d7ee118a167ffa666c2b556f50c124890048
chongliujia <jcl314159@gmail.com>
2022-11-11 22:49:07 +0800

    create a file and some codes:wq

---
+++ codes/transfer_data/pushpop.s
@@ -0,0 +1,24 @@
+.section .data
+data:
+	.int 125
+
+.section .text
+.globl main
+main:
+	movl $24420, %ecx
+	movw $350, %bx
+	movl $100, %eax
+	pushl %ecx
+	pushw %bx
+	pushl %eax
+	pushl data
+	pushl $data
+
+	popl %eax
+	popl %eax
+	popl %eax
+	popw %ax
+	popl %eax
+	movl $0, %ebx
+	movl $1, %eax
+	int $0x80
